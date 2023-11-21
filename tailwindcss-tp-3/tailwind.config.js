b75662becf19244dd317a790c867f9d02bb0dfa3
seinokoula <alexandre.charles92@gmail.com>
2023-11-21 09:14:32 +0100

    :tada: All tp for now

---
+++ tailwindcss-tp-3/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#0040e6",
+        "secondary": "#333333",
+        "tertiary": "#706f6e",
+        "title": "000000",
+      },
+      fontSize: {
+        "title": "2.5rem",
+        "subtitle": "1.5rem",
+        "body": "1rem",
+      },
+      fontWeight: {
+        "normal": 400,
+        "bold": 700,
+      },
+      borderRadius: {
+        "none": "0",
+        "sm": "0.125rem",
+        "md": "0.375rem",
+        "lg": "0.5rem",
+        "full": "9999px",
+      },
+      plugins: [],
+    },
+  }
+};
